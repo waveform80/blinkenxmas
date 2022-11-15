c0ba9f029ab9701568a14667447adc2345e72573
Dave Jones <dave@waveform.org.uk>
2022-11-15 00:30:10 +0000

    Add topic to default config

--- config.py
+++ config.py
@@ -1,10 +1,12 @@
 from mqtt_as import config
 
-config['server'] = '192.168.0.10'  # Change to suit
-
-# Not needed if you're only using ESP8266
+# WiFi configuration
 config['ssid'] = 'your_network_name'
 config['wifi_pw'] = 'your_password'
+
+# MQTT broker configuration
+config['server'] = '192.168.0.10'
+config['topic'] = 'blinkenxmas'
 
 # Configuration of the WS2812 strip
 config['led_count'] = 50

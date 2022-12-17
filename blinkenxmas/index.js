b6a5afd8d7a68007c3570363db699d98e10e01ea
Dave Jones <dave@waveform.org.uk>
2022-12-17 12:28:05 +0000

    Split forms.js up into individual units
    Each form doesn't share much (if any) code so it makes sense to split
    'em all up

---
+++ blinkenxmas/index.js
@@ -0,0 +1,102 @@
+
+function initIndexForm(form) {
+  form.querySelectorAll("ul#presets li a").forEach(
+    (link) => {
+      link.href = '#';
+      link.addEventListener('click',
+        (evt) => doShow(link.dataset.preset));
+    });
+  let manageBtn = document.createElement('input');
+  manageBtn.type = 'button';
+  manageBtn.value = 'Manage';
+  manageBtn.addEventListener('click', (evt) => doManage(form));
+  document.querySelector('#manage').replaceWith(manageBtn);
+  manageBtn.id = 'manage';
+}
+
+function doManage(form) {
+  form.querySelectorAll("ul#presets li a").forEach(
+    (link) => {
+      let check = document.createElement('input');
+      check.type = 'checkbox';
+      check.name = 'name';
+      check.dataset.preset = link.dataset.preset;
+      check.id = escapeIdent(check.dataset.preset);
+      let label = document.createElement('label');
+      label.textContent = check.dataset.preset;
+      label.htmlFor = check.id;
+      link.replaceWith(check, label);
+    });
+  let remove = document.createElement('input');
+  remove.type = 'button';
+  remove.id = 'remove';
+  remove.value = 'Remove';
+  remove.addEventListener('click', (evt) => doRemove(form));
+  let cancel = document.createElement('input');
+  cancel.type = 'button';
+  cancel.id = 'cancel';
+  cancel.value = 'Cancel';
+  cancel.addEventListener('click', (evt) => cancelManage(form));
+  form.querySelector('.buttons').replaceChildren(remove, cancel);
+}
+
+function cancelManage(form) {
+  form.querySelectorAll("ul#presets li").forEach(
+    (item) => {
+      let link = document.createElement('a');
+      link.href = '#';
+      link.dataset.preset = unescapeIdent(item.querySelector('input').id);
+      link.textContent = link.dataset.preset;
+      link.addEventListener('click',
+        (evt) => doShow(link.dataset.preset))
+      item.replaceChildren(link);
+    });
+  let create = document.createElement('a');
+  create.href = 'create.html';
+  create.classList.add('button');
+  create.textContent = 'Create';
+  let manage = document.createElement('input')
+  manage.type = 'input';
+  manage.id = 'manage';
+  manage.value = 'Manage';
+  manage.addEventListener('click', (evt) => doManage(form));
+  form.querySelector('.buttons').replaceChildren(create, manage);
+}
+
+function doRemove(form) {
+  let formList = form.querySelector('#presets');
+  let toRemove = Array
+    .from(form.elements['name'])
+    .filter((item) => item.checked);
+  let toRemoveNames = toRemove.map((item) => item.dataset.preset);
+
+  function _remove(item) {
+    if (item == undefined) {
+      cancelManage(form);
+      showMessage(`Removed ${toRemoveNames.join(', ')}`);
+    }
+    else {
+      let name = item.dataset.preset;
+      let req = new Request(`/preset/${encodeURIComponent(name)}`, {
+        method: 'DELETE',
+        cache: 'no-store',
+      });
+      return fetch(req)
+        .then(() => {
+          formList.removeChild(item.parentElement);
+          return _remove(toRemove.pop());
+        })
+        .catch((e) => showMessage(e));
+    }
+  }
+  return _remove(toRemove.pop());
+}
+
+function doShow(name) {
+  let req = new Request(`/show/${encodeURIComponent(name)}`, {
+    method: "POST",
+    cache: "no-store",
+  });
+  fetch(req)
+    .catch((e) => showMessage(e));
+}
--- blinkenxmas/messages.js
+++ blinkenxmas/messages.js
@@ -6,3 +6,30 @@ function showMessage(s) {
 function hideMessage() {
   document.querySelector("#message").style.display = "none";
 }
+
+function escapeIdent(s) {
+  let firstChar = /[A-Za-z]/;
+  let otherChar = /[A-Za-z0-9-.]/;
+  return Array.from(s).map((c, index) => {
+    if (c == ' ')
+      return '_';
+    else if (c.match(index == 0 ? firstChar : otherChar))
+      return c;
+    else
+      return ':' + c.charCodeAt(0).toString(16).padStart(4, '0');
+  }).join('');
+}
+
+function unescapeIdent(s) {
+  let charRe = /:[0-9A-Fa-f]{4}|./g;
+  return Array.from(s.matchAll(charRe)).map((match) => {
+    let c = match[0];
+    if (c == '_')
+      return ' ';
+    else if (c[0] == ':')
+      return String.fromCharCode(Number.parseInt(c.substring(1), 16));
+    else
+      return c;
+  }).join('');
+}
+
